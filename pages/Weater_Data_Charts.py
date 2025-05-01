09db750d374f28482fc0cf59604908f63c554c63
Marv41 <127249962+Marv41@users.noreply.github.com>
2025-05-01 12:48:01 -0600

    Update Weater_Data_Charts.py

--- pages/Weater_Data_Charts.py
+++ pages/Weater_Data_Charts.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from main import scatterplot3D, heatMap, barChart, dateVtemp_scatterplot, pcpVhum_scatterplot
 
 st.title("Charts")
 
@@ -13,28 +12,28 @@ tab1, tab2, tab3, tab4 = st.tabs(["3D Scatterplot", "Heatmap & Bar Chart", "Temp
 with tab1:
     st.write("This interactive 3D scatterplot visualizes the relationship between humidity and dew point in the context of precipitation. You can zoom, rotate, and select individual data points to explore patterns more closely.")
 
-    st.plotly_chart(scatterplot3D)
+    st.plotly_chart(st.session_state.scatterplot3D)
 
 with tab2:
     col1, col2 = st.columns(2)
 
     with col1:
-        st.pyplot(heatMap)
+        st.pyplot(st.session_state.heatMap)
 
     with col2:
-        st.pyplot(barChart)
+        st.pyplot(st.session_state.barChart)
 
     st.write("On the left is the **Correlation Heatmap**. This grid shows how strongly various features are correlated with one another. Values closer to **1** indicate a strong positive correlation, while values closer to **-1** indicate a strong negative correlation.")
 
     st.write("On the right is a simple **bar chart** showing the number of days with and without rain. A balanced dataset like this is better for training an effective machine learning model.")
 
 with tab3:
-    st.pyplot(dateVtemp_scatterplot)
+    st.pyplot(st.session_state.dateVtemp_scatterplot)
 
     st.write("This scatterplot shows temperature over time. As expected, lower temperatures occur during the winter months, while higher temperatures are observed in the summer.")
 
 with tab4:
-    st.pyplot(pcpVhum_scatterplot)
+    st.pyplot(st.session_state.pcpVhum_scatterplot)
 
     st.write("This scatterplot shows how humidity impacts total rainfall (precipitation). Higher levels of precipitation are typically observed when humidity exceeds 60%.")
 
